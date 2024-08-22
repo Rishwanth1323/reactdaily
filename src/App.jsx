8ab3d77fd37a902aa2564a00e9cc57fc42aabb09
Rishwanth1323 <rishwanthg1323@gmail.com>
2024-08-22 10:57:46 +0530

    22 commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,34 +9,6 @@ function App() {
     <>
 
 
-    {
-      recipe.map(
-        (each,index)=>{
-          return(
-
-            <div key={index}>
-              <Heading size={1} content={each.name} />
-              <Images src={each.image} />
-              <Heading size={3} content={"INGRIDIENTS TO PREPARE"}/>
-              <CustomList list={each.ingredients}/>
-              <Heading size={3} content={"INSTRUCTIONS"} />
-              <CustomList list={each.instructions}/>
-              {/* <Heading>THIS IS RISHWANTH</Heading>
-              <Example content={"blabla"}>
-                {/* This is a CHILDRED 
-                 */}
-                 {/* <Heading size={1} content={each.name} /> */}
-                 {/* <h1>la lala laa la</h1>
-                </Example> */} */}
-
-
-            <hr />
-
-            </div>
-          )
-        }
-      )
-    }
 
     </>
   )
