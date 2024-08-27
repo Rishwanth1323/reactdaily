07275b71eaf00fbb44975c7caa067e3aedc47ee1
Rishwanth1323 <rishwanthg1323@gmail.com>
2024-08-27 08:50:03 +0530

    Farhenheit

--- src/App.jsx
+++ src/App.jsx
@@ -1,148 +1,51 @@
-
-
-
-
 import { Component } from "react";
-import axios from "axios";
-import CardCreation from "./Components/CardCreation";
 
+class App extends Component {
+    state = {
+        fahrenheit: '', //important
+        celsius: '', // important
+    };
 
-class App extends Component{
-  state={
-    products:[],
-    categories:[]
-  }
+    changeHandler = (e) => {
+        let fahrenheit = e.target.value;
+        let celsius = (5 / 9) * (fahrenheit - 32);
 
-  componentDidMount(){
-    this.fetchCategories();
-    this.fetchProducts();
-  }
+        this.setState({
+            fahrenheit: fahrenheit,
+            celsius: celsius.toFixed(2), // Setting to 2 decimal places for clarity
+        });
+    };
 
-  fetchCategories=async()=>{
-    const categories = await axios("https://fakestoreapi.com/products/categories");
-    // console.log(categories);
-    this.setState({categories:categories.data});
-  }
-  fetchProducts=async()=>{
-    const products   = await axios("https://fakestoreapi.com/products");
-    // console.log(products);
-    this.setState({products:products.data});
-  }
-  clickHandler = (e) =>{
-    // console.log("Button clicked");
-    // console.log(e.target.innerText);
-    if(e.target.innerText==="ALL"){
-      console.log("ALL");
-      this.fetchProducts();
+    changeHandle = (e) => {
+        let celsius = e.target.value;
+        let fahrenheit = (9 / 5) * celsius + 32;
+
+        this.setState({
+            celsius: celsius,
+            fahrenheit: fahrenheit.toFixed(2), // Setting to 2 decimal places for clarity
+        });
+    };
+
+    render() {
+        return (
+            <>
+                <h1>Hello, World!</h1>
+                <h1>CONVERTER</h1>
+                <input
+                    type="text"
+                    placeholder="Fahrenheit"
+                    value={this.state.fahrenheit} // impoertant
+                    onChange={this.changeHandler}
+                /><br />
+                <input
+                    type="text"
+                    placeholder="Celsius"
+                    value={this.state.celsius}
+                    onChange={this.changeHandle}
+                />
+            </>
+        );
     }
-    else
-    this.fetchProductsByCategory(e.target.innerText); //ACtually this should be done in componentdidmount
-  }
-  fetchProductsByCategory=async(value)=>{
-    const products = await axios(https://fakestoreapi.com/products/category/${value})
-    this.setState({
-      products:products.data
-    })
-  }
-  render(){
-    return(
-        <>
-        <h1>select the Categories</h1>
-        {
-          this.state.categories.map((each,index)=>{
-            return(
-              <button onClick={this.clickHandler} key={index} class="bg-red-500 hover:bg-red-700 text-green font-bold py-2 px-4 rounded">
-                {each}
-              </button>
-            )
-          })
-        }
-        <button onClick={this.clickHandler}  class="bg-red-500 hover:bg-red-700 text-green font-bold py-2 px-4 rounded">
-                ALL
-              </button>
-        <h3>DATA THAT IS FETCHED FROM PRODUCTS</h3>
-        {
-          this.state.products.map((each,index)=>{
-            return(
-              <CardCreation key={index} title={each.title} designation={each.price} />
-            )
-          })
-        }
-        </>
-    )
-  }
 }
+
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
