a8b715dd28d6d269c2e879f1f95c63ad3cb02a58
Rishwanth1323 <rishwanthg1323@gmail.com>
2024-08-28 14:04:10 +0530

    incre decre useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import ChildComponent from './Components/ChildComponent';
-import { useState } from 'react';
-const App = () =>{
-  const [message, setMessage] = useState('');
-  const [message1, setMessage1] = useState('');
-  const handler = (data1,data2) =>{
-    setMessage(data1);
-    setMessage1(data2)
-  }
-  return(
-    <>
-    <h1>Parent Component : {message}</h1>
-    <h1>Parent Component : {message1}</h1>
-    
-    <ChildComponent pass={handler} > hello </ChildComponent>
+import { useState } from "react";
 
+
+const App = () =>{
+  const [age,setAge]=useState(10);
+  const handler=(type)=>{
+    switch(type){
+      case 'INCREMENT': 
+      setAge(age+1);
+      break;
+      case 'DECREMENT': 
+      if(age>0)
+      setAge(age-1);
+      break;
+      case 'RESET'    : setAge(0);
+      break;
+      default: alert("INVALID TYPE"); 
+      break; 
+    }
+  }
+  return (
+    <>
+    <h1>hello increment decrement using useState</h1>
+    <h1>AGE : {age}</h1>
+    <button type="button" onClick={()=>handler("INCREMENT")} >INCREMENT</button>
+    <button type="button" onClick={()=>handler("DECREMENT")}>DECREMENT</button>
+    <button type="button" onClick={()=>handler("RESET")}>RESET</button>
     </>
   )
 }
+
+
 export default App;
